94b5508cdb0d81c149ea43722af55790ce759908
chenchen04231018 <144906957+chenchen04231018@users.noreply.github.com>
2024-07-24 23:17:46 +0800

    Update RequestDexcomPermissionActivity.kt

--- plugins/source/src/main/kotlin/app/aaps/plugins/source/activities/RequestDexcomPermissionActivity.kt
+++ plugins/source/src/main/kotlin/app/aaps/plugins/source/activities/RequestDexcomPermissionActivity.kt
@@ -2,7 +2,7 @@ package app.aaps.plugins.source.activities
 
 import android.os.Bundle
 import app.aaps.core.ui.activities.TranslatedDaggerAppCompatActivity
-import app.aaps.plugins.source.DexcomPlugin
+import app.aaps.plugins.source.PathedSIAppPlugin
 
 class RequestDexcomPermissionActivity : TranslatedDaggerAppCompatActivity() {
 
@@ -10,7 +10,7 @@ class RequestDexcomPermissionActivity : TranslatedDaggerAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestPermissions(arrayOf(DexcomPlugin.PERMISSION), requestCode)
+        requestPermissions(arrayOf(PathedSIAppPlugin.PERMISSION), requestCode)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
